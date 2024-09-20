0722827b44e3cb59f9d1e01045928a2ec5dde1b9
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-09-20 00:23:05 -0700

    init

---
+++ src/siteConfig.ts
@@ -0,0 +1,16 @@
+type SiteConfiguration = {
+  title: string;
+  description: string;
+  url: string;
+  author: string;
+  figmaSrc: string;
+};
+
+export const SITE: SiteConfiguration = {
+  title: "Figma Embed Template",
+  description: "A template to embed a Figma template in.",
+  url: "https://figma-embed-template.vercel.app",
+  author: "Trevor Tyler Lee",
+  figmaSrc:
+    "https://embed.figma.com/board/wCeNBPLYqQNpmwk0haQYbx/Untitled?node-id=0-1&embed-host=share",
+};
